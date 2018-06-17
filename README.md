47f4b593c8585fd4c947f685ac5c2bf9cfea86c2
Kerem Karatal <kerem.karatal@xing.com>
2018-06-17 12:21:03 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# Artist Info service
+
+A sample service intended to demonstrate a microservices environment. This service allows one to search for movies with a specified genre and revenues higher than the provided number. The results are provided as movie ids only.
+
+## REST Endpoint
+This service exposes a REST endpoint:
+
+For batched support:
+
+``` 
+  GET /movies??genre=Drama&revenue=1000000&limit=5&offset=1
+```
+
+Here the query params are:
+
+`genre`: Matches the genre of the movie. Only single genre is allowed.
+
+`revenue`: Matches revenues greater than what is specified here.
+
+And the response is: (where the data is an array of movie ids)
+
+``` javascript
+{
+    "metadata": {
+        "offset": 1,
+        "limit": 5,
+        "total": 54
+    },
+    "data": [
+        194662,
+        449176,
+        13,
+        301337,
+        273481
+    ]
+}
+```
+
+## Docker
+Following ENV variables are available:
+
+`SERVICE_NAME`: movie-search (Default)
+
+`FAIL_PERCENT`: 0.3 (Default) - Indicates the percent of time this service will simulate a failure (500 response). This is used to simulate failures so that you can test retries, circuit breakers etc.
+
---
+++ index.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const R = require('ramda');
+const utils = require('req-res-utils');
+
+const port = process.env.PORT || 3030
+const service_name = process.env.SERVICE_NAME || 'movie-search'
+const failPercent = process.env.FAIL_PERCENT || 0.3;
+
+// Load sample data
+const movies = require('./data/movie_details3.json');
+const genres = require('./data/genres.json');
+
+const revenueFilter = revenue => {
+  return movie => movie.revenue >= revenue;
+};
+
+const genreFilter = genre => {
+  return movie => {
+    let genreObj = R.find(R.propEq('name', genre))(genres.genres);
+    if (typeof genreObj == 'undefined') {
+      throw Error(`Genre ${genre} is unknown`);
+    }
+    return R.contains({ name: genreObj.name, id: genreObj.id })(movie.genres);
+  };
+};
+
+const createMoviesResponse = (args) => {
+  const filters = [
+    {
+      "name": "revenue", 
+      "action": revenueFilter 
+    },
+    {
+      "name": "genre",
+      "action": genreFilter
+    }
+  ];
+  const filterMovies = R.curry(utils.filterItems)(args, filters);
+  const pageMovies = R.curry(utils.paging.getPagedItems)(args);
+  const pickIds = R.map(movie => movie.id)
+  return R.pipe(
+    filterMovies,
+    pickIds,
+    pageMovies,
+  )(movies);
+}
+
+// Initialize the app
+const app = express();
+
+// The GraphQL endpoint
+app.use(
+  bodyParser.json(),
+  (req, res, next) => {
+    const incomingHeaders = req.headers;
+    const headers = Object.assign(
+      utils.getCORSHeaders(),
+      utils.forwardTraceHeaders(incomingHeaders)
+    );
+    res.set(headers);
+    if (Math.random() < failPercent) {
+      // Simulate a failure
+      console.log("Failing");
+      res.sendStatus(500);
+    } else if (req.method === 'OPTIONS') {
+      res.sendStatus(200);
+    } else {
+      next();
+    }
+  }
+);
+
+// Simple restful movies endpoint
+app.get('/movies', (req, res) => {
+  try {
+    const moviesResponse = createMoviesResponse(req.query)
+    res.send(moviesResponse);
+  } catch (error) {
+    console.log(error);
+    res.send({ error: error.message });
+  }
+});
+
+// Start the server
+app.listen(3030, () => {
+  console.log(`${service_name} listening on port ${port}!`);
+  console.log(`Failure rate is set to ${failPercent}`);
+});
